3707ff1f6628943310cbe6c794f52d8e33bf1c05
= <triplicatesouls@gmail.com>
2020-11-09 20:55:59 +0400

    [update] Updated corusel component: added navigation and styles for corusel

--- src/components/Corusel/Corusel.jsx
+++ src/components/Corusel/Corusel.jsx
@@ -1,33 +1,93 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import PropTypes, { string } from "prop-types";
-import { CoruselBox, CoruselPictures, PictureWrapper } from "./CoruselElements";
+import CoruselBtn from "$components/CoruselBtn/CoruselBtn";
+import {
+  CoruselBox,
+  CoruselPictures,
+  PictureWrapper,
+  CoruselNav,
+  CoruselRadio,
+  CoruselRadios,
+} from "./CoruselElements";
 
 let propTypes = {
   pictures: PropTypes.arrayOf(string).isRequired,
 };
 
 export default function Corusel({ pictures }) {
-  let [picture, setPicture] = useState("");
-  let addRadio = () => {
-    let radio = [];
-    for (let i = 0; i < pictures.length; i++) {
-      radio.push(<input key={i} name="corusel" type="radio" />);
-    }
-    return radio;
+  let [picture, setPicture] = useState(pictures),
+    pictureRef = useRef(),
+    radioRef = useRef(),
+    nextBtnRef = useRef(),
+    prevBtnRef = useRef();
+
+  let initRadio = () => {
+    return pictures.map((el, index) => (
+      <CoruselRadio key={index}>
+        <input
+          id={`corusel_id-${index}`}
+          data-corusel-picture={index}
+          key={index}
+          onClick={slide}
+          name="corusel"
+          type="radio"
+        />
+        <label htmlFor={`corusel_id-${index}`}></label>
+      </CoruselRadio>
+    ));
   };
 
-  let slide = () => {};
+  let slide = ({ target }) => {
+    let attr = target.getAttribute("data-corusel-picture");
+    pictureRef.current.setAttribute("src", picture[attr]);
+  };
+
+  // TODO FIX it
+
+  let slideToNext = () => {
+    let attr = pictureRef.current.getAttribute("src"),
+      index = pictures.indexOf(attr);
+    console.log(index);
+    console.log(pictures.length - 1);
+    if (index === pictures.length - 1) {
+      index = index - 1;
+    }
+    pictureRef.current.setAttribute("src", picture[index + 1]);
+  };
+
+  // TODO FIX it
+
+  let slideToPrev = () => {
+    let attr = pictureRef.current.getAttribute("src"),
+      index = pictures.indexOf(attr);
+    if (index <= 0) {
+      index = 0;
+    } else {
+      index = --index;
+    }
+    pictureRef.current.setAttribute("src", picture[index]);
+  };
+
+  useEffect(() => {
+    let coruselRadio = radioRef.current.children[0].children[0];
+    coruselRadio.setAttribute("checked", "true");
+  }, []);
+
   return (
     <>
       <CoruselBox>
         <CoruselPictures>
           <PictureWrapper>
             <picture>
-              <img src={picture} alt="#" />
+              <img ref={pictureRef} src={pictures[0]} alt="#" />
             </picture>
           </PictureWrapper>
         </CoruselPictures>
-        <div className="corusel-nav">{addRadio()}</div>
+        <CoruselNav>
+          <CoruselBtn hundleClick={slideToPrev} reverse={true} />
+          <CoruselRadios ref={radioRef}>{initRadio()}</CoruselRadios>
+          <CoruselBtn hundleClick={slideToNext} />
+        </CoruselNav>
       </CoruselBox>
     </>
   );
--- src/components/Corusel/CoruselElements.jsx
+++ src/components/Corusel/CoruselElements.jsx
@@ -20,4 +20,46 @@ export let PictureWrapper = styled.div`
 
 export let CoruselPictures = styled.div`
   overflow: hidden;
+  width: 100%;
+  height: 100%;
+`;
+
+export let CoruselNav = styled.div`
+  display: flex;
+  min-width: 220px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export let CoruselRadio = styled.div`
+  box-sizing: border-box;
+  display: flex;
+  width: 14px;
+  height: 14px;
+  border-radius: 50%;
+  input[type="radio"] {
+    appearance: none;
+    opacity: 0;
+    margin: 0;
+    padding: 0;
+  }
+  input[type="radio"]:checked + label {
+    background: #519bf4;
+  }
+  input[type="radio"]:focus + label {
+    outline: 2px solid #000;
+  }
+  input[type="radio"] + label {
+    box-sizing: border-box;
+    padding: 0;
+    background: #cee1fc;
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+  }
+`;
+
+export let CoruselRadios = styled.div`
+  display: flex;
+  margin: 0 20px;
 `;
