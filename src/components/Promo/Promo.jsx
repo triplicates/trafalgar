93856f1a9f735c5bad6cca4e3ea2840b9b5adb03
tripplicate <triplicatesouls@gmail.com>
2020-11-18 23:51:34 +0400

    [update] Updated component, added isMobile state for adaptive

--- src/components/Promo/Promo.jsx
+++ src/components/Promo/Promo.jsx
@@ -1,37 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import {
-  Promo as PromoSection,
-  PromoWrapper,
-  Text,
-  Subtitle,
-  PromoLink,
-  Picture,
-} from "./PromoElements.jsx";
-
-import { Title } from "../Styles/GlobalStyles.jsx";
-import picture from "../../assets/bg.png";
 
 export default function Promo() {
   return (
-    <PromoSection>
-      <PromoWrapper>
-        <Text>
-          <Title size="big">Virtual healthcare for you</Title>
-          <Subtitle size="big">
-            Trafalgar provides progressive, and affordable healthcare,
-            accessible on mobile and online for everyone
-          </Subtitle>
-          <Router>
-            <PromoLink to="#" primary="true">
-              Consult today
-            </PromoLink>
-          </Router>
-        </Text>
-        <Picture>
-          <img src={picture} alt="Background picture" />
-        </Picture>
-      </PromoWrapper>
-    </PromoSection>
+    <section>
+      <div>
+        <p>promo</p>
+      </div>
+    </section>
   );
 }
