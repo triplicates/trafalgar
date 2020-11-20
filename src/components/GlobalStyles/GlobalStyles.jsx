2de566057f795d07b5f5a8dc40c6dbb5ee9326cc
tripplicate <triplicatesouls@gmail.com>
2020-11-20 21:03:08 +0400

    [add] Added GlobalStyles components

---
+++ src/components/GlobalStyles/GlobalStyles.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export let Wrapper = styled.div`
+  width: 1170px;
+  max-width: calc(100% - 40px);
+  margin: 0 auto;
+  height: 100%;
+`;
+
+export let Button = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: transparent;
+  border: none;
+`;
