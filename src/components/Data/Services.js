bd1222d21d4b30e83f9d1f5f167642db772388ab
tripplicates <triplicatesouls@gmail.com>
2020-11-30 20:25:21 +0400

    [add] Added Services data in folder

---
+++ src/components/Data/Services.js
@@ -0,0 +1,50 @@
+export default [
+  {
+    id: 1,
+    img: "../../assets/images/services/service1.png",
+    alt: "text",
+    title: "Search doctor",
+    subtitle:
+      "Choose your doctor from thousands of specialist, general, and trusted hospitals",
+  },
+  {
+    id: 2,
+    img: "../../assets/images/services/service2.png",
+    alt: "text",
+    title: "Online pharmacy",
+    subtitle:
+      "Buy  your medicines with our mobile application with a simple delivery system",
+  },
+  {
+    id: 3,
+    img: "../../assets/images/services/service3.png",
+    alt: "text",
+    title: "Consultation",
+    subtitle:
+      "Free consultation with our trusted doctors and get the best recomendations",
+  },
+  {
+    id: 4,
+    img: "../../assets/images/services/service4.png",
+    alt: "text",
+    title: "Details info",
+    subtitle:
+      "Free consultation with our trusted doctors and get the best recomendations",
+  },
+  {
+    id: 5,
+    img: "../../assets/images/services/service5.png",
+    alt: "text",
+    title: "Emergency care",
+    subtitle:
+      "You can get 24/7 urgent care for yourself or your children and your lovely family",
+  },
+  {
+    id: 6,
+    img: "../../assets/images/services/service6.png",
+    alt: "text",
+    title: "Details info",
+    subtitle:
+      "Free consultation with our trusted doctors and get the best recomendations",
+  },
+];
